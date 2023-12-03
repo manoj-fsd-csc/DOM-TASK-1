ec7a7770914bb18b55704a5ed4276be1b0951751
MANOJ KUMAR <143407966+manoj-fsd-csc@users.noreply.github.com>
2023-12-03 22:48:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function add() {
+    var x = document.getElementById("form");
+    var tbody = document.getElementById("tbody");
+    var tr = document.createElement("tr");
+    for ( var i = 0; i < x.length; i++) {
+      
+      var td = document.createElement("td");
+      console.log(x.elements[i].value);
+      td.innerHTML += x.elements[i].value;
+      tr.appendChild(td);
+      
+    }
+    tbody.appendChild(tr);
+    x.reset();
+  }
